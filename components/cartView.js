f43e2dcc83963f3f518d48301af74a3ca4156e2e
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-04-02 21:54:48 +0300

    Реализована форма просмотра кота

--- components/cartView.js
+++ components/cartView.js
@@ -1,18 +1,18 @@
 const generateCardView = (cat) => {
   return `<div class='cardView-popup'>
+  <div class="cardView">
   <div class="cardView-image">
  	<img src=${
     cat.image !== '' ? cat.image : defaultLink
   } alt="Картинка котика" /> 
   </div>
 	<div class="cardView-content">
-		<div class="card-name">${cat.name}</div>
-		<div class="card-description">${cat.description}</div>
-		<div class="card-age">${cat.age}</div> 
-		<div class="card-rate">${cat.rate}</div> 
-
+		<div class="card-name">Имя: ${cat.name}</div>
+		<div class="card-description">Сообщение: ${cat.description}</div>
+		<div class="card-age">Возраст: ${cat.age}</div> 
+		<div class="card-rate">Рейтинг: ${cat.rate}</div> 
+		<button class="cardView-exit"></button>
 	</div>
-	
-	<button class="cardView-exit">Закрыть</button>
-	</div>`;
+  </div>
+ </div>`;
 };
