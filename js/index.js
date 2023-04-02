a3a29bccfaf4d590753c907ac9e27221dd6bcab3
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-04-02 15:08:51 +0300

    Реализована функция просмотра

--- js/index.js
+++ js/index.js
@@ -69,10 +69,8 @@ content.addEventListener('click', (event) => {
   if (event.target.tagName === 'BUTTON') {
     switch (event.target.className) {
       case 'cat-card-view':
-        api.getDisplayCat(event.target.value).then((res) => {
-          console.log(res);
-          refreshCatsAndContent();
-        });
+        console.log(event.target.value);
+        getViewCard(event.target.value);
         break;
       case 'cat-card-update':
       // const modal = document.querySelector('.create-edit-modal-form');
@@ -96,52 +94,20 @@ content.addEventListener('click', (event) => {
       // });
       // break;
       case 'cat-card-delete':
-        deleteCatFromLocalStorage(event.target.value);
-        refreshCatsAndContent();
-        // api.getDeleteCat(event.target.value).then((res) => {//запрос на сервер и промис
-        //   console.log(res);
-        //   refreshCatsAndContent();
-        // });
+        api.getDeleteCat(event.target.value).then((res) => {
+          //запрос удаления кота на сервер
+          console.log(res);
+          refreshCatsAndContent(); //отрисовка карточек заново
+        });
         break;
     }
   }
 });
 
-// api
-//   .addCat({
-//     id: 2,
-//     name: 'Инь и Янь',
-//     favorite: false,
-//     rate: 5,
-//     age: 3,
-//     description: 'Черное и белое - единство и борьба противоположностей',
-//     image:
-//       'https://yandex-images.clstorage.net/OMpm54165/731e5dVB2A1/TciKfGsMvWYbRehj9GFlm3k7ToJJibhGvVIf_Pj2gahB727WoIjJ9rmp8bFnZUYH7EtM5Zh1SE8drl9L08K0sVjY2W77S2pzCcB9YfSgJRwua4rFXwivAC3FHOy740KpAz9uFbLfXOWoK6Vq_q1Oj-KVwDsEvpXIHW2eHMDHJPWCQlo85Jx15FXeKCb5aCeRLj3EBRrAa4gxCdUtb2vQtpPtI1N1hxXiJnB143thg4Rj2kK3zzebwFdilpsOnSp9CQXDduvf8ySb0Ght9CSmHqJ1CosTiCQIMsMXKeNqX3RfZm0SdkPboeT-uOk57wRDYNxFOQ48FIXep9zexF0u9cHHSrNr1jSkmtcqLjtjbRshcoKO1E9pDLgD3C1rLFQ-FqKtHXcA0-budXOsvGeAx-GYQjuJetNPHqBW1wtXZHkHjwiy6Vq_ZJwS5C_1oW0fYPLFhBhH7ke8jhZl4OAW9dLl7Vf7TZZj4Thy6HypBERoHU_5wzfUxJ1oWh5Hlqv4BcDItq7Tc2faWiAo-WTt3CL7hk8XAG2INs9drGgm0HcZaGWVM4UcKmV8euu8ZwtPp5MPP0WxGoDY4lUUzxBrNYdPwfblF7yikxYgI_piJthoMgxOEU6qSXwHUyOvah4-nKJtl_YL16hmOTvrN-gIyWaYDbpFuZsO1-NZF4YRbb6ICYH8Zx5wa56XbGm9aeSZpDqEidMIY0K4AR9lZCvT-Vag4NL2BZ1uZj28YD0lSs4jHMr5xH-WDFSvVdQPl2S4BEmOPq6ct60ZG2XmM21ikGk-QwwbR2wH9Ycf6WFkGfiUpWIc_c-WZel_sO43IYwLLN7HM4d30MYW5luZSFzs_ETFQfZvFPprFFuu7bar75lve8-Gkgaiz3HDEmBhqpY7WmxtVnYP0GjstHnnteFAiqQTSDDBcJQCE6xa2wxWJLPBTc_5qdp_J1Sf5WY1Lm0UJr3HjNCHJ8p2SVRmqY',
-//   })
-//   .then((res) => {
-//     console.log(res);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-const addCatInLocalStorage = (cat) => {
-  //функция добавления кота в локальное хранилище
-  store.setItem(
-    //добавляем в хранилище
-    'cats',
-    JSON.stringify([...JSON.parse(store.getItem('cats')), cat])
-  );
-};
-
-const deleteCatFromLocalStorage = (catId) => {
-  //функция удаления котика из хранилища
-  store.setItem(
-    'cats',
-    JSON.stringify(
-      JSON.parse(store.getItem('cats')).filter((el) => el.id != catId)
-    )
-  );
+const getViewCard = (id) => {
+  let view = JSON.parse(store.getItem('cats'));
+  let viewCard = view.find((el) => el['id'] == id);
+  console.log(viewCard);
 };
 
 const getNewIdOfCat = () => {
